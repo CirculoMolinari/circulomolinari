bc6f3aa15025af4fbf226a6a82ca77b78f8e99fb
Eduardo <edu@192.168.1.11>
2024-09-27 15:53:53 +0200

    feat: send form data to endpoint

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,16 +1,18 @@
 import { Resend } from 'resend';
 import { NextRequest } from 'next/server';
+import ContactMeEmail from '@/components/contact';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req: NextRequest) {
   try {
+    const { name, emailAddress, phoneNumber, content } = await req.json()
+    
     const { data, error } = await resend.emails.send({
-      from: 'eduardo.mathias@protonmail.com',
-      to: 'circulomolinari@protonmail.com',
-      subject: 'Inscripción',
-      text: 'Ejemplo'
-      //react: EmailTemplate({ firstName: 'John' }),
+      from: emailAddress,
+      to: ['circulomolinari@protonmail.com'],
+      subject: `Inscripción de ${name}`,
+      react: ContactMeEmail({ name, emailAddress, phoneNumber, content }),
     });
 
     if (error) {
